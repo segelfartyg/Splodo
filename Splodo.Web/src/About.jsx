302748405ee7d502239e42694513cd351f75925e
segelfartyg <samuel.swaren@outlook.com>
2023-08-05 16:23:59 +0200

    add: metadata and fixed about page

--- Splodo.Web/src/About.jsx
+++ Splodo.Web/src/About.jsx
@@ -6,32 +6,31 @@ export default function About() {
     <div className="About">
       <div className="infoArea card">
         <h1>What is Splodo?</h1>
+        
         <p>
-          Splodo is a platform made for creating value through text components
-          that you create yourself. Have you ever wanted to express something in
-          words, and create further value with it?{" "}
-          <strong>We bet you have.</strong>
+          Splodo is a platform made for creating value through text components that you
+          create through a web interface.
         </p>
 
         <p>
           Even if Splodo can be used for anything, the fundamental purpose with
-          the Splodo project is about celebrating great ideas. The project came
-          to reality after the founder of the initiative, Samuel Swarén wanted
-          to gather his own golden moments when experiencing neat things. The
-          main goal of the project is to develop a community driven by ideas,
-          appriciation, and hype.
+          the Splodo project is to celebrate and encourage great ideas. The
+          long term goal of the project is to develop a community driven by these ideas. 
+          The philosophy of Splodo is based on creativity and appriciation of innovation. This mainly in 
+          a Software Development context.
         </p>
 
         <p>
-          If the concept of Splodos and how and when to use them seems abstract
-          for you, the community pages given by the developers of the platform
-          is a good place to start.
+          Furthermore, another fundemental part of Splodo is to create an open platform
+          where everyone can create and utilize Splodos, to create value in other environments through
+          the Splodo Backend API. At worst, Splodo is still a very open and fun way of showing off your
+          social media links :P
         </p>
 
-        <h3>
-          However, Splodo shows its best side when you are taking use of it. In
-          whatever way you want to.{" "}
-        </h3>
+        <p>
+          This project is made and maintained by Samuel Swarén, a Software Engineer located in 
+          Uppsala, Sweden. 
+        </p>
       </div>
     </div>
   );
